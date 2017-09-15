278bc55ecf8a2d694b56fe3940598422c76cb52b
Francisco Castellanos <Francisco Castellanos>
2017-09-14 20:10:08 -0500

    miniproy

---
+++ Miniproy/miniproy.c
@@ -0,0 +1,21 @@
+#import <stdio.h>
+#import <stdlib.h>
+
+
+int cp(const void*a,const void*b){
+    return *(int*)a>*(int*)b;
+}
+int arrayPairSum(int* nums, int numsSize) {
+	int compare_int( const void* a, const void* b ) {
+        return *(int*)a>*(int*)b;
+    }
+    
+    qsort(nums, numsSize, sizeof(int), compare_int );
+    
+    int result = 0;
+    for (int i = 0; i < numsSize; i += 2) {
+        result += nums[i];
+    }
+    
+    return result;
+}
