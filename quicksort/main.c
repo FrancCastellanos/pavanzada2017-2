a96fca07b25e1bdaa1242feec313963b30b091ab
Francisco Castellanos <Francisco Castellanos>
2017-10-12 19:38:06 -0500

    Qicksort 2.0

--- quicksort/main.c
+++ quicksort/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "quicksort.h"
 #include <string.h>
+#define MAX_LENGTH_ORDER 20
+#define STUDENTS_LENGTH 3
 
 typedef struct s{
 	char *name;
@@ -9,23 +11,33 @@ typedef struct s{
 }Student;
 
 /* Add function signatures here */
-int cmpNames(void *element1, void *element2){
-	Student student1 = (Student *) element1;
-	Student student2 = (Student *) element2;
+int cmpStudents(void const *element1, void const *element2, int attribute){
+  Student student1 = (Student *) element1;
+  Student student2 = (Student *) element2;
 
-	return strcmp(student1->name, student2->name);
+  switch(attribute) {
+    case 0: //name
+      return strcmp(student1->name, student2->name);
+    case 1: //matricula
+      return strcmp(student1->matricula, student2->matricula);
+  }
+}
+
+int cmpNames(void const *element1, void const *element2){
+
+	return strcmp(element1,element2,0);
 }
 
 int cmpMatriculas( void const *element1, void const *element2){
-	Student student1 = (Student *) element1;
-	Student student2 = (Student *) element2;
-
-	return strcmp(student1->matricula - student2->matricula);
+	return strcmp(element1, element2, 1);
 }
 
 int main(int argc, char **argv) {
-  char order[20];
-  Student group[3];
+  char order[MAX_LENGTH_ORDER];
+  int i;
+  Student key;
+  Student *foundStudent; 
+  Student group[STUDENTS_LENGTH];
 
   group[0].name = strdup("juanito");
   group[0].matricula = 17;
@@ -50,6 +62,17 @@ int main(int argc, char **argv) {
 
   for(i=0; i<3; i++){
   	printf("Student %s %d \n", gorup[i].name,group[i].matricula);
+  }
+
+  printf("Which is the id: ");
+  scanf("%d", &neededStudent);
+  key.matricula = neededStudent;
+
+  foundStudent = bsearch(&key,group,STUDENTS_LENGTH,sizeof(Student),cmpMatriculas);
+  if(foundStudent != NULL){
+    printf("Student %s %d \n", foundStudent->name, foundStudent->matricula);
+  } else {
+    printf("Student not found with id %d \n", neededStudent);
   }
 
   return 0;
