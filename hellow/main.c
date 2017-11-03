014cade289e60bf965f93ba6d0038d5a9f4a05bf
Francisco Castellanos <Francisco Castellanos>
2017-11-02 20:31:38 -0600

    Fix race condition

--- hellow/main.c
+++ hellow/main.c
@@ -1,17 +1,26 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <semaphore.h>
 #include "hellow.h"
 
-#define NUM_THREADS 10000
+#define NUM_THREADS 1000
 
 unsigned int total;
+sem_t sem1;
 
 void *add100(void *threadid) {
     long tid;
     tid = (long)threadid;
+    unsigned int localTotal;
     printf("Adding  100 #%ld!\n", tid);
-    total +=100;
+    sem_wait(&sem1);
+    localTotal = total;
+    localTotal += 100;
+    //sleep(tid%3);
+    total = localTotal;
+    sem_post(&sem1);
     pthread_exit(NULL);
  }
 
@@ -20,6 +29,7 @@ void *add100(void *threadid) {
     int rc;
     long t;
     total = 0;
+    sem_init(&sem1, 0, 1);
     printf("Initial total = %d \n", total);
     for(t=0; t<NUM_THREADS; t++){	
        printf("In main: creating thread %ld\n", t);
