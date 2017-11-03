154c355fdec909123d80c508732d898ad84474cb
Francisco Castellanos <Francisco Castellanos>
2017-11-02 20:58:55 -0600

    Phil lifestyle

--- hellow/main.c
+++ hellow/main.c
@@ -5,35 +5,48 @@
 #include <semaphore.h>
 #include "hellow.h"
 
-#define NUM_THREADS 1000
+#define NUM_THREADS 10
 
 unsigned int total;
-sem_t sem1;
+sem_t forks[NUM_THREADS];
 
-void *add100(void *threadid) {
+void thinking(int n){
+	sleep(rand()%n);
+}
+
+void eating(int n){
+	sleep(rand()&n);
+}
+
+void *lifeStyle(void *threadid) {
     long tid;
     tid = (long)threadid;
-    unsigned int localTotal;
-    printf("Adding  100 #%ld!\n", tid);
-    sem_wait(&sem1);
-    localTotal = total;
-    localTotal += 100;
-    //sleep(tid%3);
-    total = localTotal;
-    sem_post(&sem1);
-    pthread_exit(NULL);
+
+    while(1){
+    	thinking(3);
+    	printf("I am phil %lu, I am hungry \n", tid);
+    	sem_wait(&forks [tid]);
+    	sem_wait(&forks [(tid + 1) %NUM_THREADS]);
+    	eating(4);
+    	sem_post(&forks [tid]);
+    	sem_post(&forks [(tid + 1) %NUM_THREADS]);
+    	printf("I am phil %lu, I am thinking \n", tid);
+    }
+    
  }
 
  int main (int argc, char *argv[]) {
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
-    total = 0;
-    sem_init(&sem1, 0, 1);
-    printf("Initial total = %d \n", total);
+
+    for(t=0; t<NUM_THREADS; t++){
+    	sem_init(&forks[t],0,1);	
+    }
+
     for(t=0; t<NUM_THREADS; t++){	
        printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, add100, (void *)t);
+       rc = pthread_create(&threads[t], NULL, lifeStyle, (void *)t);
        if (rc){
           printf("ERROR; return code from pthread_create() is %d\n", rc);
           exit(-1);
@@ -43,8 +56,6 @@ void *add100(void *threadid) {
 
     	pthread_join(threads[t],NULL);
 	}
-    printf("Final total = %d \n", total);
-
 
     /* Last thing that main() should do */
     pthread_exit(NULL);
