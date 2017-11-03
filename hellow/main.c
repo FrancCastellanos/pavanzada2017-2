6df5064c75ccb5d516b51ff7ed9d4446579ac3b8
Francisco Castellanos <Francisco Castellanos>
2017-11-02 19:44:44 -0600

    total

--- hellow/main.c
+++ hellow/main.c
@@ -3,14 +3,15 @@
 #include <stdio.h>
 #include "hellow.h"
 
-#define NUM_THREADS     5
+#define NUM_THREADS 10000
 
-/* Add function signatures here */
+unsigned int total;
 
-void *PrintHello(void *threadid) {
+void *add100(void *threadid) {
     long tid;
     tid = (long)threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
+    printf("Adding  100 #%ld!\n", tid);
+    total +=100;
     pthread_exit(NULL);
  }
 
@@ -18,14 +19,22 @@ void *PrintHello(void *threadid) {
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
-    for(t=0; t<NUM_THREADS; t++){
+    total = 0;
+    printf("Initial total = %d \n", total);
+    for(t=0; t<NUM_THREADS; t++){	
        printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+       rc = pthread_create(&threads[t], NULL, add100, (void *)t);
        if (rc){
           printf("ERROR; return code from pthread_create() is %d\n", rc);
           exit(-1);
-       }
+       }   
     }
+    for(t=0; t<NUM_THREADS; t++){
+
+    	pthread_join(threads[t],NULL);
+	}
+    printf("Final total = %d \n", total);
+
 
     /* Last thing that main() should do */
     pthread_exit(NULL);
